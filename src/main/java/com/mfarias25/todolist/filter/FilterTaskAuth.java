fed0b8a84a76084103efca6176bc5e346d4a55eb
Mateus Farias <mateusfariasdev@outlook.com>
2023-10-11 17:55:41 -0300

    basic decoded

--- src/main/java/com/mfarias25/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/mfarias25/todolist/filter/FilterTaskAuth.java
@@ -1,21 +1,45 @@
 package com.mfarias25.todolist.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 @Component // toda class que queremos que o spring gerencie
-public class FilterTaskAuth implements Filter  {
-    @Override
-    public void doFilter(ServletRequest request,
-                         ServletResponse response,
-                         FilterChain chain)
-            throws IOException, ServletException {
-        //Executar alguma ação
+public class FilterTaskAuth extends OncePerRequestFilter {
 
-        System.out.println("Chegou no filtro");
-        chain.doFilter(request, response);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        var authorization = request.getHeader("Authorization");
+        var authEncoded = authorization.substring("Basic".length()).trim(); //subtring = int unico ou dois ints. Pege Basica e calcule qual o length, tira os caracteres e espaços
+
+        byte[] authDecoded = Base64.getDecoder().decode(authEncoded); //Decode converter/converter um array de bytes
+
+        var authString = new String(authDecoded);
+
+
+
+
+        // 0 -> ["farias3"], 1 -> ["999333222"]
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+        System.out.println("Authorization: " +"Username: "+ username + " " + "Password: " + password);
+
+        //Pegar a autenticação(usuario e senha)
+        //Validar usuario
+        //Validar senha
+        //Tudo ok, segue!
+
+        filterChain.doFilter(request, response);
 
     }
 }
