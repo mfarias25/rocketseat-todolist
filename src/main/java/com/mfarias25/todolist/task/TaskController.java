edb6a5f951f311c5246527975976aa5c08d37813
Mateus Farias <mateusfariasdev@outlook.com>
2023-10-12 18:20:55 -0300

    get method(lista de taferas)

--- src/main/java/com/mfarias25/todolist/task/TaskController.java
+++ src/main/java/com/mfarias25/todolist/task/TaskController.java
@@ -4,12 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -43,7 +41,10 @@ public class TaskController {
                 .body(task);
     }
 
-
-
-
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
 }
