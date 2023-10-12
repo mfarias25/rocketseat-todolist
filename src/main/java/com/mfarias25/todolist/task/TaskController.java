cd33a43a3b320576e5e5ecae95fec714ccbfadb6
Mateus Farias <mateusfariasdev@outlook.com>
2023-10-12 17:55:35 -0300

    validando horas

--- src/main/java/com/mfarias25/todolist/task/TaskController.java
+++ src/main/java/com/mfarias25/todolist/task/TaskController.java
@@ -2,11 +2,14 @@ package com.mfarias25.todolist.task;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -17,14 +20,30 @@ public class TaskController {
     private TaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         System.out.println("Chegou no controller");
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
+        var currentDate = LocalDateTime.now();
+        //Ex: 10/11/2023 - currentDate
+        // 10/10/2023 - startAt
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("A data de inicio/data de termino deve ser maior que a data atual");
+        }
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("A data de inicio deve ser menor que a data de término");
+        }
         var task = this.taskRepository.save(taskModel);
-        return task;
-
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(task);
     }
+
+
 
 
 }
